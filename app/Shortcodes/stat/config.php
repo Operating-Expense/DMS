9bf1c8a678b7aeee10cb983dd7ac56470e428bf5
vvv3 <test.vvv@ukr.net>
2019-03-02 20:16:47 +0200

    statistic

---
+++ app/Shortcodes/stat/config.php
@@ -0,0 +1,66 @@
+<?php
+
+return array(
+	'name'     => esc_html__( 'Statistic', 'dms' ),
+	'base'     => 'stat',
+	'category' => esc_html__( 'DMS', 'dms' ),
+	'params'   => array(
+		// ============================ Block Settings =============================
+		array(
+			'type'       => 'textfield',
+			'param_name' => 'title',
+			'heading'    => esc_html__( 'Title', 'dms' ),
+			'value'      => '',
+			'group'      => esc_html__( 'General', 'dms' ),
+		),
+		array(
+			'type'       => 'textfield',
+			'heading'    => esc_html__( 'CSS classes', 'dms' ),
+			'param_name' => 'el_classes',
+			'value'      => '',
+			'group'      => esc_html__( 'General', 'dms' ),
+		),
+		array(
+			'type'       => 'el_id',
+			'heading'    => esc_html__( 'CSS id', 'dms' ),
+			'param_name' => 'el_id',
+			'settings'   => array(
+				'auto_generate' => true,
+			),
+			'group'      => esc_html__( 'General', 'dms' ),
+		),
+		
+		// ========================== Block Items Settings ===========================
+		array(
+			'type'       => 'param_group',
+			'param_name' => 'items',
+			'heading'    => esc_html__( 'Statistic', 'dms' ),
+			'group'      => esc_html__( 'Statistic', 'dms' ),
+			'value'      => '',
+			'params'     => array(
+				
+				array(
+					'type'       => 'textfield',
+					'param_name' => 'item_title',
+					'heading'    => esc_html__( 'Item title', 'dms' ),
+					'value'      => '',
+					'admin_label' => true
+				),
+				array(
+					'type'       => 'textarea',
+					'param_name' => 'item_text',
+					'heading'    => esc_html__( 'Item text', 'dms' ),
+					'value'      => '',
+					//'admin_label' => true
+				),
+				array(
+					'type'       => 'textfield',
+					'param_name' => 'item_el_classes',
+					'heading'    => esc_html__( ' Item CSS classes', 'dms' ),
+					'value'      => '',
+				),
+			)
+		),
+	
+	)
+);
---
+++ app/Shortcodes/stat/view/view.php
@@ -0,0 +1,70 @@
+<?php
+
+use DMS\Helper\Media;
+
+$atts = $data['atts'];
+
+$attributes = $classes = array();
+
+$classes[] = 'shortcode-stat';
+if ( ! empty( $atts['el_classes'] ) ) {
+	$classes[] = esc_html( $atts['el_classes'] );
+}
+
+$attributes[] = ! empty( $atts['el_id'] ) ? 'id="' . $atts['el_id'] . '"' : '';
+$attributes[] = ! empty( $classes ) ? 'class="' . implode( ' ', $classes ) . '"' : '';
+
+?>
+<section <?php echo implode( $attributes ); ?>>
+	
+	
+	<?php if ( ! empty( $atts['title'] ) ) { ?>
+		<div class="row">
+			<div class="col-12">
+				<h2 class="s-stat-title">
+					<?php echo $atts['title'] ?>
+				</h2>
+			</div>
+		</div>
+	<?php } ?>
+	
+	<?php if ( ! empty( $atts['items_arr'] ) ) { ?>
+		
+		<div class="row">
+			
+			<?php
+			foreach ( $atts['items_arr'] as $item ) {
+				
+				$item_title = ! empty( $item['item_title'] ) ? esc_html( $item['item_title'] ) : '';
+				$item_text  = ! empty( $item['item_text'] ) ? esc_html( $item['item_text'] ) : '';
+				$item_class = ! empty( $item['item_el_classes'] )
+					? ' ' . esc_attr( $item['item_el_classes'] )
+					: '';
+				
+				?>
+				
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					
+					<div class="s-stat <?php echo $item_class ?>">
+						
+						<div class="s-stat__body">
+							<div class="s-stat__title">
+								<?php echo $item_title ?>
+							</div>
+							<div class="s-stat__text">
+								<?php echo $item_text ?>
+							</div>
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			<?php } ?>
+		
+		</div>
+	
+	<?php } ?>
+
+</section>
